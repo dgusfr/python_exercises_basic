834e676dac4f91ec517e06a9e01f030a5d847c04
dgusfr <diegogustavo.fr@gmail.com>
2024-10-15 09:49:48 -0300

    fweat(primo)

--- EstruturaRepeticao/12.py
+++ EstruturaRepeticao/12.py
@@ -1,3 +1,20 @@
+"""
+INICIO
+LER N
+Para cada i de 2 ate N faça:
+    p = true
+    Se N% i == 0:
+        "Numero não é primo"
+        p == false
+    Caso contrario:
+        "Numero é primo"
+
+FIM
+
+Note que se desejarmos determinar se um numero muito grande como 1.256.876 é primo a complexidade do algoritmo aumenta muit.
+Para melhorar isso usamos o loop até a raiz quadrada do numeor em questão.
+"""
+
 def eh_primo(numero):
     if numero < 2:
         return False
@@ -7,6 +24,7 @@ def eh_primo(numero):
     return True
 
 numero = int(input("Digite um número inteiro: "))
+
 if eh_primo(numero):
     print(f"{numero} é um número primo.")
 else:
