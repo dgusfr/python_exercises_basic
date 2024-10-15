9e16b5b9e36026be70018911964359d51c609d33
dgusfr <diegogustavo.fr@gmail.com>
2024-10-15 10:13:08 -0300

    feat(delete)

--- EstruturaRepeticao/15.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Digite o valor de N para a série: "))
-soma_serie = 0
-
-for i in range(1, n + 1):
-    soma_serie += i / (2 * i - 1)
-
-print(f"Série até o {n}-ésimo termo: {soma_serie}")
