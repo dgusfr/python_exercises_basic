1f7cbb99c1d1c08c59fa35c11d1126e3efa57238
dgusfr <diegogustavo.fr@gmail.com>
2024-10-15 09:42:18 -0300

    feat(fatorial)

--- EstruturaRepeticao/10.py
+++ EstruturaRepeticao/10.py
@@ -1,7 +1,7 @@
 numero = int(input("Digite um número para calcular o fatorial: "))
 fatorial = 1
 
-for i in range(2, numero):
+for i in range(2, numero + 1):
     fatorial *= i
 
 print(f"{numero}! = {fatorial}")
