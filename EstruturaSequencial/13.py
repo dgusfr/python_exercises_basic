1c79e33d5596074983881c30f4ed08b5576e20e0
dgusfr <diegogustavo.fr@gmail.com>
2024-10-16 07:36:35 -0300

    delete

--- EstruturaSequencial/13.py
+++
@@ -1,4 +0,0 @@
-altura = float(input("Digite sua altura em metros: "))
-peso_ideal_homem = (72.7 * altura) - 58
-peso_ideal_mulher = (62.1 * altura) - 44.7
-print(f"Peso ideal para homem: {peso_ideal_homem} kg\nPeso ideal para mulher: {peso_ideal_mulher} kg")
